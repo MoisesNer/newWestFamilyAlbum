956a215f839254479c28df5fb93a50bc251edc76
Moises Nervis <StarCommand@Moisess-iMac.local>
2021-04-26 12:56:07 -0700

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>New West Gallery</h1>
+      <h2>Our Pictures</h2>
+      <p>Family Album to share capture beautiful moments in our lives, and within them, the family, friends and even pets that mean the most to us. To help us remember the good times, and keep in our hearts and minds experiences we might otherwise forget.</p>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+ var firebaseConfig = {
+    
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage();
+  const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+
+  export { projectStorage, projectFirestore, timestamp }
